6b70b43318886eaa4e3346739e62e897e230e43b
Juraj Marcin <xmarcin@fi.muni.cz>
2023-03-12 13:09:10 +0100

    Add library management module

---
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/LibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementApplication.class, args);
+    }
+
+}
---
+++ library-management/src/test/java/cz/muni/fi/pa165/seminar3/librarymanagement/LibraryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
