c778d34c515e6f1c76b03a50b96275f1501b8d14
Juraj Marcin <xmarcin@fi.muni.cz>
2023-03-12 13:09:37 +0100

    Add payment gate module

---
+++ payment-gate/src/main/java/cz/muni/fi/pa165/seminar3/paymentgate/PaymentGateApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.paymentgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentGateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentGateApplication.class, args);
+    }
+
+}
---
+++ payment-gate/src/test/java/cz/muni/fi/pa165/seminar3/paymentgate/PaymentGateApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.paymentgate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
