b1a3fa511568f196305090b1cfaf467f8c52deb6
Juraj Marcin <xmarcin@fi.muni.cz>
2023-03-20 17:38:19 +0100

    Add reporting and self-service kiosk modules

---
+++ self-service-kiosk/src/main/java/cz/muni/fi/pa165/seminar3/selfservicekiosk/SelfServiceKioskApplication.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.selfservicekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfServiceKioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfServiceKioskApplication.class, args);
+    }
+
+}
