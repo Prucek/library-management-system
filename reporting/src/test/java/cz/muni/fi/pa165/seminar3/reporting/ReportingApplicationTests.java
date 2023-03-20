3f057e891d21e8ffe38c8448e808b4ff941fb01d
Juraj Marcin <xmarcin@fi.muni.cz>
2023-03-20 17:38:19 +0100

    Add reporting and self-service kiosk modules

---
+++ reporting/src/test/java/cz/muni/fi/pa165/seminar3/reporting/ReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ self-service-kiosk/src/test/java/cz/muni/fi/pa165/seminar3/selfservicekiosk/SelfServiceKioskApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.selfservicekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfServiceKioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
