49243de5c43b003e72ceca3b7267b48dc78a5481
Marek <marek.micek@azet.sk>
2023-03-22 11:31:30 +0100

    Created borrowing mapper and repository interfaces

---
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/borrowing/BorrowingMapper.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.borrowing;
+
+import cz.muni.fi.pa165.seminar3.librarymanagement.common.DomainMapper;
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.borrowing.BorrowingDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Represents borrowing mapper between Borrowing entity and Borrowing DTO
+ * @author Marek Miček
+ */
+@Mapper
+public interface BorrowingMapper extends DomainMapper<Borrowing, BorrowingDto> {
+}
---
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/borrowing/BorrowingRepository.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.borrowing;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Represents borrowing JPA repository
+ * @author Marek Miček
+ */
+@Repository
+public interface BorrowingRepository extends JpaRepository<Borrowing, String> {
+}
