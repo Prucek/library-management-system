e2df4ef8ad467919062898ca87761699acd127d0
Marek <marek.micek@azet.sk>
2023-03-22 11:33:25 +0100

    Created borrowing DTO

---
+++ model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/borrowing/BorrowingDto.java
@@ -0,0 +1,26 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.borrowing;
+
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.common.DomainObjectDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * Represents borrowing DTO
+ * @author Marek Miček
+ */
+@Getter
+@Setter
+public class BorrowingDto extends DomainObjectDto {
+
+    private LocalDateTime from;
+
+    private LocalDateTime to;
+
+//      private BookInstanceDto bookInstance;
+//
+//      private UserDto user;
+//
+//      private FineDto fine
+}
