15545699ab8b8c6048d7792fce5c36fcc7390aa4
Marek <marek.micek@azet.sk>
2023-03-22 11:35:30 +0100

    Created reservation mapper and repository interfaces

---
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/reservation/ReservationMapper.java
@@ -0,0 +1,13 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.reservation;
+
+import cz.muni.fi.pa165.seminar3.librarymanagement.common.DomainMapper;
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.reservation.ReservationDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Represents reservation mapper between Reservation entity and Reservation DTO
+ * @author Marek Miček
+ */
+@Mapper
+public interface ReservationMapper extends DomainMapper<Reservation, ReservationDto> {
+}
---
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/reservation/ReservationRepository.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.reservation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Represents reservation JPA repository
+ * @author Marek Miček
+ */
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, String> {
+}
