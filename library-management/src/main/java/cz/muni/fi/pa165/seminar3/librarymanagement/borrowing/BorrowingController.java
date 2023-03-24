b13bcd84cdb885fec60e1ff53ef328f5656ab3df
Marek <marek.micek@azet.sk>
2023-03-24 11:33:39 +0100

    Borrowing GET unsuccessful now throws 404 instead of 500

--- library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/borrowing/BorrowingController.java
+++ library-management/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/borrowing/BorrowingController.java
@@ -10,9 +10,12 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Spring REST Controller for borrowing service.
@@ -62,7 +65,13 @@ public class BorrowingController {
     )
     @GetMapping("/{id}")
     public BorrowingDto find(@PathVariable String id) {
-        return mapper.toDto(service.find(id));
+        Borrowing borrowing;
+        try {
+            borrowing = service.find(id);
+        } catch (EntityNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "borrowing with id=" + id + " not found");
+        }
+        return mapper.toDto(borrowing);
     }
 
     /**
