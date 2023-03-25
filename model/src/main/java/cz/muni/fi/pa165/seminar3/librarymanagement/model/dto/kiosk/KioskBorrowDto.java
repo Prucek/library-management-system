0e44e0703a9ac5681939fac6ae25f97eec2d815f
Marek Fiala <541700@mail.muni.cz>
2023-03-25 20:30:38 +0100

    Kiosk Comments and OpenAPI

--- model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/kiosk/KioskBorrowDto.java
+++ model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/kiosk/KioskBorrowDto.java
@@ -3,6 +3,9 @@ package cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.kiosk;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * DTO for a borrowing action at kiosk.
+ */
 @Getter
 @Setter
 public class KioskBorrowDto {
--- self-service-kiosk/src/main/java/cz/muni/fi/pa165/seminar3/selfservicekiosk/KioskController.java
+++ self-service-kiosk/src/main/java/cz/muni/fi/pa165/seminar3/selfservicekiosk/KioskController.java
@@ -8,12 +8,32 @@ import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.borrowing.Borrowing
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.kiosk.KioskBorrowDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.kiosk.KioskReturnDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.user.UserDto;
-import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * REST API for actions around kiosk.
+ */
+
 @RestController
+@OpenAPIDefinition(
+        info = @Info(title = "RestAPI controller for borrowing and returning at kiosk",
+                version = "1.0",
+                contact = @Contact(name = "Marek Fiala", email = "xfiala6@fi.muni.cz")
+        )
+)
 @RequestMapping("/kiosk")
 public class KioskController {
 
@@ -26,7 +46,21 @@ public class KioskController {
 //        this.bookService = bookService;
 //    }
 
+
+    /**
+     * User can borrow a book at kiosk
+     */
+    @Operation(
+            summary = "Borrow a book at kiosk",
+            description = "New borrowing is made based on incoming BookInstanceId and USerId data",
+            responses = {
+                    @ApiResponse(responseCode = "202", ref = "#/components/responses/KioskBorrowResponse")
+//                    Todo: response
+//                    @ApiResponse(responseCode = "400", description = "input data were not correct"),
+            }
+    )
     @PostMapping("/borrow")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public BorrowingDto borrow(@RequestBody KioskBorrowDto dto){
         BorrowingDto newBorrowing = new BorrowingDto();
 //      Todo:  Find book instance by its id
@@ -39,9 +73,21 @@ public class KioskController {
         return newBorrowing;
     }
 
+    /**
+     * User can return a book at kiosk
+     */
+    @Operation(
+            summary = "Return a book at kiosk",
+            description = "Returning is made based on incoming BookInstanceId data",
+            responses = {
+                    @ApiResponse(responseCode = "202", ref = "#/components/responses/KioskReturnResponse")
+//                    Todo: response
+//                    @ApiResponse(responseCode = "400", description = "input data were not correct"),
+            }
+    )
     @PostMapping("/return")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public BorrowingDto return_book(@RequestBody KioskReturnDto dto){
-        System.out.println(dto.getBookInstanceId());
         BorrowingDto newBorrowing = new BorrowingDto();
 //      Todo:  Find book instance by its id
 
