d7ae6a11018ca10e1cad5c5dfd20ceaae37bca2b
Marek <marek.micek@azet.sk>
2023-03-26 22:06:11 +0200

    Added equal methods in kiosk DTOs for test purposes

--- model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/kiosk/KioskReturnDto.java
+++ model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/kiosk/KioskReturnDto.java
@@ -3,6 +3,8 @@ package cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.kiosk;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 /**
  * DTO for a returning action at kiosk.
  */
@@ -10,4 +12,11 @@ import lombok.Setter;
 @Setter
 public class KioskReturnDto {
     private String bookInstanceId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof KioskReturnDto that)) return false;
+        return Objects.equals(bookInstanceId, that.bookInstanceId);
+    }
 }
