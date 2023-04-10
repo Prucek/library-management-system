41632044d2961c01b6fd905c752cd223b415e43b
Marek Miček <marek.micek@azet.sk>
2023-04-10 18:53:13 +0200

    Created BorrowingCreateDto class used by layers

---
+++ model/src/main/java/cz/muni/fi/pa165/seminar3/librarymanagement/model/dto/borrowing/BorrowingCreateDto.java
@@ -0,0 +1,38 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.borrowing;
+
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+
+/**
+ * Class representing borrowing create dto.
+ *
+ * @author Marek Miček
+ */
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowingCreateDto {
+
+    private LocalDateTime from;
+
+    private LocalDateTime to;
+
+    private LocalDateTime returned;
+
+    @NotBlank
+    private String bookInstanceID;
+
+    @NotBlank
+    private String userID;
+
+    @NotBlank
+    private String fineID;
+}
