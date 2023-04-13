e2951aead3a78d55b9244efd0e36e8fb1a940b09
Peter Rúček <xrucek@fi.muni.cz>
2023-04-13 10:10:04 +0200

    Created DataInitializer for payment-gate

---
+++ payment-gate/src/main/java/cz/muni/fi/pa165/seminar3/paymentgate/DataInitializer.java
@@ -0,0 +1,35 @@
+package cz.muni.fi.pa165.seminar3.paymentgate;
+
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.paymentgate.TransactionStatus;
+import cz.muni.fi.pa165.seminar3.paymentgate.transaction.Transaction;
+import cz.muni.fi.pa165.seminar3.paymentgate.transaction.TransactionService;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Data initializer for the payment gate.
+ */
+@Component
+public class DataInitializer implements ApplicationRunner {
+
+    private final TransactionService transactionService;
+
+    public DataInitializer(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) {
+        Transaction transaction = Transaction.builder()
+                .status(TransactionStatus.APPROVED)
+                .amount(100)
+                .build();
+        transactionService.create(transaction);
+
+        Transaction transaction2 = Transaction.builder()
+                .amount(45.45)
+                .build();
+        transactionService.create(transaction2);
+    }
+}
