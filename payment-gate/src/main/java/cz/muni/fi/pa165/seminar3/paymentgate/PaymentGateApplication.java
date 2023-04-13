a32b993efd7602b54218f5e8ba5f77eba389e843
Peter Rúček <xrucek@fi.muni.cz>
2023-04-13 10:11:13 +0200

    Javadoc for PaymentGateApplication

--- payment-gate/src/main/java/cz/muni/fi/pa165/seminar3/paymentgate/PaymentGateApplication.java
+++ payment-gate/src/main/java/cz/muni/fi/pa165/seminar3/paymentgate/PaymentGateApplication.java
@@ -3,6 +3,9 @@ package cz.muni.fi.pa165.seminar3.paymentgate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of the payment gate application.
+ */
 @SpringBootApplication
 public class PaymentGateApplication {
 
