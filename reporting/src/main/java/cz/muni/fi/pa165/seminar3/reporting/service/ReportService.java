46a23b40588c3a9cd249a85faabc9ceaccaad3b4
Marek Miček <marek.micek@azet.sk>
2023-04-14 16:17:31 +0200

    Completed report service logic

--- reporting/src/main/java/cz/muni/fi/pa165/seminar3/reporting/service/ReportService.java
+++ reporting/src/main/java/cz/muni/fi/pa165/seminar3/reporting/service/ReportService.java
@@ -2,127 +2,174 @@ package cz.muni.fi.pa165.seminar3.reporting.service;
 
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.borrowing.BorrowingDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.common.Result;
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.fine.FineDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.user.UserDto;
+import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.user.UserType;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
+/**
+ * Represents report service which implements reports of users, books and fines.
+ *
+ * @author Marek Miček
+ */
 @Service
 public class ReportService {
 
-    // Ideally, the path to the service should be obtained from a configuration file
-    public final WebClient client = WebClient.create("http://localhost:8080/");
+    public final WebClient client;
+
+    @Autowired
+    public ReportService(WebClient client) {
+        this.client = client;
+    }
 
     /**
      * Generates report with number of fines and total paid fines of specific user.
+     *
      * @param userId Specifies user for whom the report is generated
      * @return ReportDto object with number of fines and total paid fines of specific user
      */
     public FinanceReportDto generateFinanceReport(String userId) {
-        // TODO implement logic for for getting real values, when fine dto and service will be created
-        // TODO uncomment when all four major services can communicate among them
-        // So far use mock values
-
         FinanceReportDto report = new FinanceReportDto();
-//        UserDto user = client.get()
-//                .uri(uriBuilder -> uriBuilder.pathSegment("users", userId).build())
-//                .retrieve()
-//                .bodyToMono(UserDto.class)
-//                .block();
+        UserDto user = client.get()
+                .uri(uriBuilder -> uriBuilder.pathSegment("users", userId).build())
+                .retrieve()
+                .bodyToMono(UserDto.class)
+                .block();
+
+        List<FineDto> fines = getUserFines(userId);
+        double totalPaid = 0.0;
+
+        for (var fine : fines) {
+            totalPaid += fine.getAmount();
+        }
 
         report.setGeneratedAt(Instant.now());
-        //report.setUser(user);
-        report.setFinesCount(5);
-        report.setFinesTotalPaid(100);
+        report.setUser(user);
+        report.setFinesCount(fines.size());
+        report.setFinesTotalPaid(totalPaid);
 
         return report;
     }
 
     /**
      * Generate report with number of users in system.
+     *
      * @return ReportDto object with set number of users in system
      */
     public UserReportDto generateUserReport() {
-        // TODO uncomment when all four major services can communicate among them
-        // So far use mock values
-
         UserReportDto report = new UserReportDto();
 
-//        List<UserDto> users = new ArrayList<>();
-//        Result<UserDto> usersPage;
-//        int page = 0;
+        List<UserDto> users = new ArrayList<>();
+        Result<UserDto> usersPage;
+        int page = 0;
 
-//        while (true) {
-//            int finalPage = page;
-//
-//            usersPage = client
-//                    .get()
-//                    .uri(uriBuilder -> uriBuilder.path("/users").queryParam("page", finalPage).build())
-//                    .retrieve()
-//                    .bodyToMono(new ParameterizedTypeReference<Result<UserDto>>() {})
-//                    .block();
-//
-//            if (noMoreUsers(usersPage)) {
-//                break;
-//            }
-//
-//            users.addAll(usersPage.getItems());
-//            page++;
-//        }
+        while (true) {
+            int finalPage = page;
+
+            usersPage = client
+                    .get()
+                    .uri(uriBuilder -> uriBuilder.path("/users").queryParam("page", finalPage).build())
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Result<UserDto>>() {})
+                    .block();
+
+            if (noMoreUsers(usersPage)) {
+                break;
+            }
+
+            users.addAll(usersPage.getItems());
+            page++;
+        }
 
         report.setGeneratedAt(Instant.now());
-        // report.setUsersCount(users.size());
-        report.setUsersCount(10);
-        report.setLibrarianCount(2);
-        report.setNewUserCount(1);
+        report.setUsersCount(getUsersCount(users));
+        report.setLibrarianCount(users.size() - report.getUsersCount());
+        report.setNewUserCount(getNewUsersCount(users));
 
         return report;
     }
 
     /**
      * Generates report with number of borrowed and returned books of specific user.
+     *
      * @param userId Specifies user for whom the report is generated
      * @return ReportDto object with number of borrowed and returned books of specific user
      */
     public BookReportDto generateBookReport(String userId) {
-        // TODO uncomment when all four major services can communicate among them
-        // So far use mock values
-
         BookReportDto report = new BookReportDto();
 
-//        UserDto user = client.get()
-//                .uri(uriBuilder -> uriBuilder.pathSegment("users", userId).build())
-//                .retrieve()
-//                .bodyToMono(UserDto.class)
-//                .block();
-//
-//        List<BorrowingDto> borrowings = getUserBorrowings(userId);
-//        int returnedBookCounter = 0, borrowedBookCounter = 0;
-//
-//        for (var borrowing : borrowings) {
-//            if (borrowing.getTo() != null) {
-//                returnedBookCounter++;
-//            }
-//            borrowedBookCounter++;
-//        }
+        UserDto user = client.get()
+                .uri(uriBuilder -> uriBuilder.pathSegment("users", userId).build())
+                .retrieve()
+                .bodyToMono(UserDto.class)
+                .block();
+
+        List<BorrowingDto> borrowings = getUserBorrowings(userId);
+        int returnedBookCounter = 0;
+        int borrowedBookCounter = 0;
+
+        for (var borrowing : borrowings) {
+            if (borrowing.getTo() != null) {
+                returnedBookCounter++;
+            }
+            borrowedBookCounter++;
+        }
 
         report.setGeneratedAt(Instant.now());
-        // report.setUser(user);
-        // report.setBorrowedBooksCount(borrowedBookCounter);
-        // report.setReturnedBooksCount(returnedBookCounter);
-        report.setBorrowedBooksCount(1);
-        report.setReturnedBooksCount(1);
+        report.setUser(user);
+        report.setBorrowedBooksCount(borrowedBookCounter);
+        report.setReturnedBooksCount(returnedBookCounter);
 
         return report;
     }
 
     /**
+     * Returns all fines of specific user.
+     *
+     * @param userId Specifies user for whom the fines are returned
+     * @return List of fines for specific user
+     */
+    private List<FineDto> getUserFines(String userId) {
+        List<FineDto> fines = new ArrayList<>();
+        Result<FineDto> finesPage;
+        int page = 0;
+
+        while (true) {
+            int finalPage = page;
+
+            finesPage = client
+                    .get()
+                    .uri(uriBuilder -> uriBuilder.path("/fines").queryParam("page", finalPage).build())
+                    .retrieve()
+                    .bodyToMono(new ParameterizedTypeReference<Result<FineDto>>() {})
+                    .block();
+
+            if (noMoreFines(finesPage)) {
+                break;
+            }
+
+            fines.addAll(finesPage.getItems()
+                    .stream()
+                    .filter(r -> r.getIssuer().getId().equals(userId))
+                    .collect(Collectors.toList()));
+
+            page++;
+        }
+
+        return fines;
+    }
+
+    /**
      * Returns all borrowings of specific user.
+     *
      * @param userId Specifies user for whom the borrowings are returned
      * @return List of borrowings for specific user
      */
@@ -157,7 +204,18 @@ public class ReportService {
     }
 
     /**
-     * Checks if there left some user on specific page
+     * Checks if there left some fines on specific page.
+     *
+     * @param finesPage Actual page which is checked
+     * @return          Flag which represent whether there left some more fines
+     */
+    private boolean noMoreFines(Result<FineDto> finesPage) {
+        return finesPage == null || finesPage.getItems() == null || finesPage.getItems().isEmpty();
+    }
+
+    /**
+     * Checks if there left some users on specific page.
+     *
      * @param usersPage Actual page which is checked
      * @return Flag which represent whether there left some more users
     */
@@ -166,11 +224,44 @@ public class ReportService {
     }
 
     /**
-     * Checks if there left some borrowings on specific page
+     * Checks if there left some borrowings on specific page.
+     *
      * @param borrowingsPage Actual page which is checked
      * @return Flag which represent whether there left some more borrowings
      */
     private boolean noMoreBorrowings(Result<BorrowingDto> borrowingsPage) {
         return borrowingsPage == null || borrowingsPage.getItems() == null || borrowingsPage.getItems().isEmpty();
+    }
+
+    /**
+     * Counts number of users (clients not librarians) in system.
+     *
+     * @param users List of all users in system
+     * @return      Count of clients in system
+     */
+    private int getUsersCount(List<UserDto> users) {
+        int userCounter = 0;
+        for (var user : users) {
+            if (user.getUserType() == UserType.CLIENT) {
+                userCounter++;
+            }
+        }
+        return  userCounter;
+    }
+
+    /**
+     * Counts number of new users in system (account is not older than 3 days).
+     *
+     * @param users List of all users in system
+     * @return      Count of new users in system
+     */
+    private int getNewUsersCount(List<UserDto> users) {
+        int newUserCounter = 0;
+        for (var user : users) {
+            if (user.getCreatedAt().isAfter(LocalDateTime.now().minusDays(3))) {
+                newUserCounter++;
+            }
+        }
+        return  newUserCounter;
     }
 }
