4e2a96524a68adb862085045689a61df350fdcea
Peter Rúček <xrucek@fi.muni.cz>
2023-04-15 15:44:27 +0200

    Added update tests for FineFacadeImplTests

--- library-management/src/test/java/cz/muni/fi/pa165/seminar3/librarymanagement/fine/FineFacadeImplTests.java
+++ library-management/src/test/java/cz/muni/fi/pa165/seminar3/librarymanagement/fine/FineFacadeImplTests.java
@@ -7,11 +7,13 @@ import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.fine.FineCreateDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.model.dto.fine.FineDto;
 import cz.muni.fi.pa165.seminar3.librarymanagement.user.User;
 import cz.muni.fi.pa165.seminar3.librarymanagement.user.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -83,7 +85,90 @@ public class FineFacadeImplTests {
     public void createFineNullCreateDto() {
 
         FineCreateDto fineCreateDto = null;
-
         assertThrows(NullPointerException.class, () -> fineFacade.create(fineCreateDto));
+    }
+
+    @Test
+    @Transactional
+    public void updateFineSuccessful() {
+
+        // first create a fine
+        FineDto result = createFine();
+
+        // now update the fine
+        Fine fine = domainRepository.findById(result.getId()).get();
+
+        FineCreateDto newCreateDto = FineCreateDto.builder()
+                .amount(15.6)
+                .issuerId(fine.getIssuer().getId())
+                .outstandingBorrowingId(fine.getOutstandingBorrowing().getId())
+                .build();
+        given(userService.find(any(String.class))).willReturn(fine.getIssuer());
+        given(borrowingService.find(any(String.class))).willReturn(fine.getOutstandingBorrowing());
+
+        result = fineFacade.update(fine.getId(), newCreateDto);
+
+        assertThat(domainRepository.findById(result.getId())).isPresent();
+        assertThat(domainMapper.fromDto(result).getAmount()).isEqualTo(newCreateDto.getAmount());
+    }
+
+    @Test
+    public void updateFineEmptyCreateDto() {
+
+        // first create a fine
+        FineDto result = createFine();
+
+        // now update the fine
+        FineCreateDto fineCreateDto = FineCreateDto.builder().build();
+        Fine fakeFine = Fine.builder().build();
+
+        given(userService.find(any(String.class))).willReturn(null);
+        given(borrowingService.find(any(String.class))).willReturn(null);
+
+        result = fineFacade.update(result.getId(),fineCreateDto);
+        assertThat(domainMapper.fromDto(result)).isEqualTo(fakeFine);
+        assertThat(domainRepository.findById(result.getId())).isPresent();
+    }
+
+    @Test
+    public void updateFineNullId() {
+
+        FineCreateDto fineCreateDto = FineCreateDto.builder().build();
+        assertThrows(InvalidDataAccessApiUsageException.class, () -> fineFacade.update(null, fineCreateDto));
+    }
+
+    @Test
+    public void updateFineNonExistentId() {
+
+        FineCreateDto fineCreateDto = FineCreateDto.builder().build();
+        assertThrows(EntityNotFoundException.class, () -> fineFacade.update("non-existent", fineCreateDto));
+    }
+
+    @Test
+    public void updateFineNullCreateDto() {
+
+        // first create a fine
+        FineDto result = createFine();
+
+        // now update the fine
+        FineCreateDto fineCreateDto = null;
+        assertThrows(NullPointerException.class, () -> fineFacade.update(result.getId(), fineCreateDto));
+    }
+
+    private FineDto createFine() {
+        FineCreateDto fineCreateDto =
+                FineCreateDto.builder().amount(10.).issuerId("random").outstandingBorrowingId("random").build();
+        Borrowing fakeBorrowing = fakeBorrowing(faker);
+        User fakeUser = fakeBorrowing.getUser();
+        Fine fakeFine = Fine.builder().amount(10.).issuer(fakeUser).outstandingBorrowing(fakeBorrowing).build();
+
+        given(userService.find(any(String.class))).willReturn(fakeUser);
+        given(borrowingService.find(any(String.class))).willReturn(fakeBorrowing);
+
+        FineDto result = fineFacade.create(fineCreateDto);
+        assertThat(domainMapper.fromDto(result)).isEqualTo(fakeFine);
+        assertThat(domainRepository.findById(result.getId())).isPresent();
+
+        return result;
     }
 }
