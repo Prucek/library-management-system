fc3a839153ea1f0b684f8db4f0a68e09f8e1bb2b
Juraj Marcin <xmarcin@fi.muni.cz>
2023-04-16 18:36:41 +0200

    library: Add generic domain service tests

---
+++ library-management/src/test/java/cz/muni/fi/pa165/seminar3/librarymanagement/common/TestEntity.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.seminar3.librarymanagement.common;
+
+import lombok.experimental.SuperBuilder;
+
+/**
+ * Class representing a test entity.
+ *
+ * @author Juraj Marcin
+ */
+@SuperBuilder
+public class TestEntity extends DomainObject {
+}
